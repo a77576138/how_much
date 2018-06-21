e4c94e0f7b41acb8cbd9fbe957163eef715ef8fd
a77576138 <dennis871026@gmail.com>
2018-06-22 04:24:28 +0800

    完成

---
+++ how much/how much/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace how_much
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string homework2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog bbb = new Microsoft.Win32.OpenFileDialog();
+
+            Nullable<bool> result = bbb.ShowDialog();
+
+            if (result == true)
+            {
+                homework2 = bbb.FileName;
+
+                string[] lines = System.IO.File.ReadAllLines(homework2);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split('|');
+
+                    no2 item = new no2();
+
+                    item.Day.Text = parts[0];
+                    item.ItemName.Text = parts[1];
+                    item.ItemPrice.Text = parts[2];
+
+                    ToDoList.Children.Add(item);
+                }
+            }
+        }
+
+
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            List<string> homework2s = new List<string>();
+
+            foreach (no2 item in ToDoList.Children)
+            {
+                string homework2 = "";
+
+                homework2 += item.Day.Text + "|" + item.ItemName.Text + "|" + item.ItemPrice.Text;
+
+                homework2s.Add(homework2);
+            }
+
+            try
+            {
+                System.IO.File.WriteAllLines(homework2, homework2s);
+            }
+            catch
+            {
+                Microsoft.Win32.SaveFileDialog bbb = new Microsoft.Win32.SaveFileDialog();
+
+                Nullable<bool> result = bbb.ShowDialog();
+
+                if (result == true)
+                {
+                    homework2 = bbb.FileName;
+
+                    System.IO.File.WriteAllLines(homework2, homework2s);
+                }
+            }
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            int all = 0;
+
+            foreach (no2 item in ToDoList.Children)
+            {
+                all += item.itemPriceValue;
+            }
+
+            total.Text = all.ToString();
+
+        }
+
+        private void plus_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            no2 item = new no2();
+            item.Delete += new EventHandler(Delete);
+
+            ToDoList.Children.Add(item);
+        }
+        private void Delete(object sender, EventArgs e)
+        {
+            ToDoList.Children.Remove((no2)sender);
+        }
+    }
+}
---
+++ how much/how much/no2.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace how_much
+{
+    /// <summary>
+    /// item.xaml 的互動邏輯
+    /// </summary>
+    public partial class no2 : UserControl
+    {
+
+        public no2()
+        {
+            InitializeComponent();
+        }
+
+        public int itemPriceValue
+        {
+            get
+            {
+                try
+                {
+                    return int.Parse(ItemPrice.Text);
+                }
+                catch
+                {
+                    MessageBox.Show("請輸入數字");
+                    return 0;
+                }
+            }
+            set
+            {
+                ItemPrice.Text = value.ToString();
+            }
+        }
+
+        public event EventHandler Delete;
+
+        private void itemPrice_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (ItemPrice.Text == "" && e.Key == Key.Back)
+            {
+                Delete(this, null);
+            }
+        }
+
+        private void Day_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Day.Text = "";
+        }
+
+        private void Day_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Day.Text == "")
+                Day.Text = DateTime.Now.ToString("M / dd");
+        }
+
+        private void Day_Loaded(object sender, RoutedEventArgs e)
+        {
+            Day.Text = DateTime.Now.ToString("M / dd");
+        }
+
+        private void ItemName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ItemName.Text = "";
+        }
+
+        private void ItemName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (ItemName.Text == "")
+                ItemName.Text = "New Task";
+        }
+
+        private void ItemPrice_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ItemPrice.Text = "";
+        }
+
+        private void ItemPrice_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (ItemPrice.Text == "")
+                ItemPrice.Text = "0";
+        }
+    }
+}
